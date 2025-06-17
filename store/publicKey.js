41768b62849ab1033b64f0d98b32fadea903a65b
kaka <cherry_flutter@proton.me>
2025-06-17 10:55:41 +0800

    公钥处理：传参

--- store/publicKey.js
+++ store/publicKey.js
@@ -1,30 +1,43 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 import JSEncrypt from 'jsencrypt'
-
 import { useRequest, COMMON_API_PATH } from '../fetch'
 
+// 写死的 RSA 公钥
+const STATIC_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6bx3Hhiy1JsWakIlO4iQXJB7E
+D6gwMRikLI1NEYGPZHQ/R8y95/N2zQfqt3RE9hyeuBGajWHS31vOrH8/spSNMLRL
+sw2YCJgUlglESFQUGe2jLqCxDnAfSuFgAPpA3pUgDLF1kySoZ5n08nTtgGFkXUevw
+eOUidIfUNaQswSqCwIDAQAB
+-----END PUBLIC KEY-----`
+
 export const usePublicKeyStore = defineStore('publicKey', () => {
-    const { response: publicKey, onRefresh: onUpdatePublicKey } = useRequest({
+    const keyCache = ref('')
+    const { response, onRefresh } = useRequest({
         url: COMMON_API_PATH.PUBLIC_KEY,
         initialValues: ''
     })
 
-    const onEncode = async(content) => {
-        if (!publicKey.value) await onUpdatePublicKey()
+    const onEncode = async (content, useStatic = false) => {
+        let keyToUse = ''
+
+        if (useStatic) {
+            keyToUse = STATIC_PUBLIC_KEY
+        } else {
+            if (!keyCache.value) {
+                await onRefresh()
+                keyCache.value = response.value
+            }
+            keyToUse = keyCache.value
+        }
 
         const encrypt = new JSEncrypt()
-        encrypt.setPublicKey(publicKey.value)
-
+        encrypt.setPublicKey(keyToUse)
         return encrypt.encrypt(content)
     }
 
     return {
-        publicKey,
         onEncode,
-        onUpdatePublicKey
+        onUpdatePublicKey: onRefresh,
     }
-}, {
-    persist: {
-        storage: localStorage,
-    },
 })
